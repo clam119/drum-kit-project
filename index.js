17ec5c1d9d2df05886fb2d70d9004d6120ebf80f
Clam119 <Christopher.lam119@gmail.com>
2022-03-27 22:17:45 +0100

    Learnt about anonymous (unnamed functions) and the 'this' keyword to find out what type of event happened.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-
 for (var i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function () {
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
 
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        this.style.color="white";
 
     });
+
 }
+
+// var tom1 = new Audio('sounds/tom-1.mp3');
+// tom1.play();
