7d3de9e200c174a9e34fbfd9b753cac7fbab45a7
Clam119 <Christopher.lam119@gmail.com>
2022-03-29 15:37:38 +0100

    Add animation (setTimeout function) to buttons when event triggered.

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
         var buttonInnerHTML = this.innerHTML;
+
         makeSound(buttonInnerHTML);
 
+        buttonAnimation(buttonInnerHTML);
     });
 
 }
@@ -16,6 +18,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 document.addEventListener("keydown", function (event) {
 
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 
 });
 
@@ -59,3 +63,16 @@ function makeSound(key) {
 
     };
 };
+
+//Adding animation to the buttons
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100);
+
+}
